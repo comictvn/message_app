009e3c8d8eca38b86f772b3a663bcd88ee3c1ea9
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-09 19:26:04 +0700

    add functions in MentionHelper.java

--- src/main/java/helpers/string/MentionHelper.java
+++ src/main/java/helpers/string/MentionHelper.java
@@ -1,7 +1,17 @@
 package helpers.string;
 
+import classes.Mention;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * this class contains all functions help us to work with "Mentions"
  */
 public class MentionHelper {
+
+    public static List<Mention> getMentionsFromString(String stringInput){
+        List<Mention> mentions = new ArrayList<Mention>();
+        return mentions;
+    }
 }
