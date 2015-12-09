0e82219f0d546e02964562b47fe300eb57876b8b
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-09 19:50:47 +0700

    add class JsonConverter.java

---
+++ src/main/java/helpers/json/JsonConverter.java
@@ -0,0 +1,13 @@
+package helpers.json;
+
+
+import classes.Message;
+
+public class JsonConverter {
+
+//    public static json convertMessageToJson(Message message){
+//        Json json = ...
+//        TODO: convert message to json then return it
+//        return json;
+//    }
+}
