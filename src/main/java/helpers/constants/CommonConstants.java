504b1a7d3fea190649ccedfbb839f8eded77b6d4
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-09 20:35:27 +0700

    add class CommonConstants.java

---
+++ src/main/java/helpers/constants/CommonConstants.java
@@ -0,0 +1,12 @@
+package helpers.constants;
+
+import java.util.regex.Pattern;
+
+public class CommonConstants {
+
+    public static final Pattern URL_PATTERN = Pattern.compile(
+            "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+                    + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+                    + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+}
