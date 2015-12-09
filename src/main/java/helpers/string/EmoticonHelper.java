baf45a69cbd55a5bc9468c44823f4d2688c3d296
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-09 19:25:06 +0700

    add classes EmoticonHelper.java, LinkHelper.java

---
+++ src/main/java/helpers/string/EmoticonHelper.java
@@ -0,0 +1,17 @@
+package helpers.string;
+
+import classes.Emoticon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * this class contains all functions help us to work with "Emoticons"
+ */
+public class EmoticonHelper {
+
+    public static List<Emoticon> getEmoticonsFromString(String stringInput){
+        List<Emoticon> emoticons = new ArrayList<Emoticon>();
+        return emoticons;
+    }
+}
---
+++ src/main/java/helpers/string/LinkHelper.java
@@ -0,0 +1,23 @@
+package helpers.string;
+
+import classes.Link;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * this class contains all functions help us to work with "Links"
+ */
+public class LinkHelper {
+
+    public static List<Link> getLinksFromString(String stringInput){
+        List<Link> links = new ArrayList<Link>();
+        return links;
+    }
+
+    public static String getTitleFromURL(URL url){
+        String title = "";
+        return title;
+    }
+}
