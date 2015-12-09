f1f664f49fd141afb01e2c2aaea41c6b2f83f9c9
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-09 21:00:01 +0700

    some changes in LinkHelper.java

--- src/main/java/helpers/string/LinkHelper.java
+++ src/main/java/helpers/string/LinkHelper.java
@@ -5,20 +5,31 @@ import classes.Link;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import static helpers.constants.CommonConstants.*;
 
 /**
  * this class contains all functions help us to work with "Links"
  */
 public class LinkHelper {
 
-    public static List<Link> getLinksFromString(String stringInput){
+    public static List<Link> getLinksFromString(String stringInput) throws Exception{
         List<Link> links = new ArrayList<Link>();
-        // TODO: need to parse String and get All Links
+        List<URL> urls = parseURLsFromString(stringInput);
+        for(URL url : urls){
+            links.add(new Link(url));
+        }
         return links;
     }
 
-    public static String getTitleFromURL(URL url){
-        String title = "";
-        return title;
+    private static List<URL> parseURLsFromString (String stringInput) throws Exception{
+        List<URL> urls = new ArrayList<URL>();
+        Matcher matcher = URL_PATTERN.matcher(stringInput);
+        while (matcher.find()) {
+            int matchStart = matcher.start(1);
+            int matchEnd = matcher.end();
+            urls.add(new URL(stringInput.substring(matchStart, matchEnd)));
+        }
+        return urls;
     }
 }
