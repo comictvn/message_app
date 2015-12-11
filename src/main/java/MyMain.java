54d01860c8179ecc88beebaff56f7cd953499dd3
Phong Tat <phongtattuan@gmail.com>
2015-12-11 15:11:55 +0700

    Parse emoticon

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,7 @@
-
 public class MyMain {
-
-    public static String sampleFunction(){
-        return "";
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
     }
 }
--- src/main/java/helpers/string/EmoticonHelper.java
+++ src/main/java/helpers/string/EmoticonHelper.java
@@ -1,18 +1,25 @@
 package helpers.string;
 
-import classes.Emoticon;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+
+import classes.Emoticon;
+import helpers.constants.CommonConstants;
 
 /**
  * this class contains all functions help us to work with "Emoticons"
  */
 public class EmoticonHelper {
 
-    public static List<Emoticon> getEmoticonsFromString(String stringInput){
-        List<Emoticon> emoticons = new ArrayList<Emoticon>();
-        // TODO: need to parse String and get All emoticons
-        return emoticons;
-    }
+	public static List<Emoticon> getEmoticonsFromString(String stringInput) {
+		List<Emoticon> emoticons = new ArrayList<Emoticon>();
+		Matcher matcher = CommonConstants.EMOTICON_PATTERN.matcher(stringInput);
+		while (matcher.find()) {
+			int matchStart = matcher.start(1);
+			int matchEnd = matcher.end() - 1;
+			emoticons.add(new Emoticon(stringInput.substring(matchStart, matchEnd)));
+		}
+		return emoticons;
+	}
 }
